b49c441301176f064c752f9b4b7e6d6fd7e33c19
JacksonSol <94706006+JacksonSol@users.noreply.github.com>
2022-10-03 20:05:48 +0300

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,11 @@
+package javaCoreHW3;
+
+public class Apple extends Fruit{
+
+    public Apple(float weight) {
+        super(weight);
+    }
+    public Apple() {
+        super();
+    }
+}
---
+++ Fruit.java
@@ -0,0 +1,13 @@
+package javaCoreHW3;
+
+public abstract class Fruit {
+    private final float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
