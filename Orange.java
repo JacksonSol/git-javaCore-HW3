a4811e723918bf0e52a4cbfd87d06ad672887ea1
JacksonSol <94706006+JacksonSol@users.noreply.github.com>
2022-10-04 21:10:20 +0300

    Add files via upload

---
+++ Orange.java
@@ -0,0 +1,11 @@
+package javaCoreHW3;
+
+public class Orange extends Fruit {
+    public Orange(float weight) {
+        super(weight);
+    }
+
+    public Orange() {
+        super(1.5f);
+    }
+}
